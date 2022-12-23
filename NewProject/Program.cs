9c3eb4623c9a565332432dc3ed3c4406cb8d9fc0
Yusif <yusifyp@code.edu.az>
2022-12-23 19:31:30 +0400

    mmaster

---
+++ NewProject/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace NewProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("New Repository");
+
+        }
+    }
+}
